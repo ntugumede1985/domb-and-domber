9c1c97659f6dbe964450a6f2756710accf5c781c
Anathi Anathi <anathi.gumede@codeimmersives.com>
2019-10-09 15:33:13 -0400

    possible solutions

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-function addNodeElement (paragraph){
-    const node = document.createElement("LI");
-    const textNode = document.textNode("para");
-    node.appendChild(para);
+function addNodeElement (newLi){
+    document.querySelector('ul')
+    const newLi = document.createElement('li');
+    addNodeElement(newLi)
+    document.appendChild(newLi)
 }
+
+function addNodeElement (newUl){
+    document.querySelector('#lorem');
+    const newUl = document.createElement('ul');
+    addNodeElement(newUl)
+    document.appendChild(newUl)
+}
+
+function returnText (addedText){
+    const hello = document.createElement('li');
+    hello.innerText = addedText;
+    return hello;
+}
+
+function imageString (sourceImage){
+    
+}
+
+//const newLi = document.createElement('li');
+//addNodeElement(newLi)
